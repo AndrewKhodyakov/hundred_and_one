42462fbd1d1b30d749759424c0a0cd763cd8622f
ajet <andrew.khodyakov@bostongene.com>
2018-04-17 20:11:55 +0300

    Continue write loader

--- data_loader/loader.py
+++ data_loader/loader.py
@@ -26,23 +26,15 @@ class Loader:
         self._db = None
         self._tmp_folder = os.environ.get('TMP_FOLDER', '../tmp')
         self._retry_timeout = int(os.environ.get('RETRY_TIMEOUT', '10'))
+        self._complite = False
 
         #here check attributes
-        if not from_date and not to_date:
-            raise AttributeError('FROM_DATE and TO_DATE envs are not defined')
+        if from_date and to_date:
+            self._period['from'] = from_date
+            self._period['to'] = to_date
+            self._period['ranges'] = self._check_and_split_period()
         else:
-            for date in [from_date, to_date]:
-                if len(date) != 8:
-                    raise ValueError('Too small date value {}'.foramt(date))
-
-                month = date[4:6]
-                if month[0] == '0':
-                    month = month[1]
-
-                day = date[-2:]
-                if day[0] == '0':
-                    day = day[1]
-                self._period[date] = datetime.date(year=date[:4], month=month, day=day)
+            raise AttributeError('FROM_DATE or TO_DATE envs are not defined')
 
         if not self._base_url:
             raise AttributeError('SOURSE_URL env is not defined')
@@ -55,11 +47,61 @@ class Loader:
 
         self._logger.info('Created : {}'.foramt(self))
 
+
+    def _check_and_split_period(self):
+        """
+        validate datetime values and split period
+        """
+        out = []
+        tmp = dict.fromkeys(self._period.keys())
+        if len(set(self._period.values())) == 1:
+            raise ValueError('Dates are should`t be equal {}'.foramt(self._period))
+
+        for key in self._period.keys():
+            data = self._period.get(key)
+            if len(date) != 8:
+                raise ValueError('Too small date value {}'.foramt(date))
+            month = date[4:6]
+            if month[0] == '0':
+                month = month[1]
+            
+            day = date[-2:]
+            if day[0] == '0':
+                day = day[1]
+            
+            tmp[key] = datetime.date(year=date[:4], month=month, day=day)
+
+        delta = tmp['to'] - tmp['from']
+        last = tmp['from']
+        for day in range(1, delta.days + 1):
+            _ = tmp['from'] + datetime.datetime(day=day)
+            if _.month != last.month:
+                month = last.month
+                if last.month < 10:
+                    month = '0' + str(last.month)
+                out.append(str(last.year) + month)
+                last = _
+
+        self._logger.debug('Perod splited at : {}'.foramt(out))
+        return out
+
     def run(self):
         """
         run loader
         """
-        pass
+        while not self._complite:
+
+            try:
+                #try do all actions
+                self._load_data()
+                self._sent_data_to_db()
+                self._complite = True
+            except ConnectionError as con_err:
+                self._logger.error('Can`t complite cycle error: {}, station: {}'.\
+                        format(con_err, self))
+
+            self._logger.info('Whait {} sec, and try again...'.foramt(self._retry_timeout))
+            time.sleep(self._retry_timeout)
 
     def _get_db_connect(self):
         """
@@ -68,41 +110,52 @@ class Loader:
         self._logger.info('{} - try to create connection to db... '.format(self))
 
     @property
-    def urls(self):
+    def _urls(self):
         """
         Build urls
         """
-        out = None
-
-#        if len(self._period) == 1:
-#            out = [self._base_url + '/' + self._period.get(key)[:-2] + '01' \
-#                for key in self._period.keys()]
-#
-#        else:
-#            tmp = list(self._period.keys())
-#            start = None
-#            if self._period.get(tmp[0]) > self._period.get(tmp[1]):
-#                start = tmp[0]
-#            else:
-#                start = tmp[1]
-#
-#        for url in out:
-#            if 
-
+        out = []
+        for inst in self._period.get('ranges'):
+            tmp = datetime.date(year=inst[:4])
+            prefix = '01.rar'
+            if tmp.year < 2009:
+                prefix = '01.zip'
+            out.append(self._base_url + '/' + inst + prefix)
         return out
             
     def _load_data(self):
         """
+        load and store data in tmp volume
         """
         self._logger.info('{} - start load data from source... '.format(self))
+        for url in self._urls:
+            self._logger.debug('Try get data by {}...'.format(url))
+
+            resp = requests.get(url)
+
+            #if server return not 200, we should check params
+            if not resp.ok:
+                raise ConnectionError(\
+                    'Bad request params, check ENVs - loader {}, req - {}'.\
+                        format(self, resp.text))
+
+            with open(url.split('/')[-1:][0], 'wb') as output:
+                for chank in resp:
+                    output.write(chank)
+                self._logger.debug('File {}, whas succesful saved'.format(output))
+
+    def _sent_data_to_db:
+        """
+        """
+        pass
 
     def __repr__(self):
         """
         pretty print
         """
-        return "\n\tLoader ::url {}::db ::folder {}::timeout {}::period {}"\
+        return "\n\tLoader ::url {}::db ::folder {}::timeout {}::period {}, {}"\
             .format(self._base_url, self._db_url, self._tmp_folder, \
-                self._retry_timeout, self._period)
+                self._retry_timeout, self._period.get('from'), self._period.get('to'))
 
 
 if __name__ == "__main__":
