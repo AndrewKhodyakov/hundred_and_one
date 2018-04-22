5c6197ccd565a0c179cf6122e559b5101a0e4103
ajet <andrew.khodyakov@bostongene.com>
2018-04-23 01:57:35 +0300

    Ready api

--- api/src/test_api.py
+++ api/src/test_api.py
@@ -2,9 +2,13 @@ import os
 import json
 import logging
 import unittest
+import datetime
 from sqlalchemy import (Table, Column, Numeric, Integer, String, DateTime, MetaData, ForeignKey)
-from settings import ENGINE
+
+from settings import ENGINE, DB
 from app import app
+from models import OneHundredReport
+from utils import model_to_dict
 
 get_level = lambda: logging.DEBUG if os.environ.get('DEBUG') else logging.INFO
 logging.basicConfig(level=get_level())
@@ -52,20 +56,41 @@ class APITestCase(unittest.TestCase):
             'ORP': 0, 'OVP': 0, 'OITGP': 0.0, 'IR': 23064358, 'IV': 0, 'IITG': 23064358.0,\
             'DT': datetime.date(2010, 5, 1), 'PRIZ': 1}
 
-    def test_a_models(self):
+    def test_a_models_and_utils(self):
         """
         test OneHundredReport model
         """
         logging.info('\nStart model test..')
+        inst = OneHundredReport(**self._data)
+        DB.add(inst)
+        res = DB.query(OneHundredReport).all()
+        model = res.pop()
+        self.assertEqual(str(model), '1_1_2010-05-01')
+        self.assertDictEqual(model_to_dict(model), \
+            {'REGN': 1, 'PLAN': 'А', 'NUM_SC': '10207', 'A_P': '2', 'VR': 23064358, 'VV':\
+            0, 'VITG': 23064358.0, 'ORA': 0, 'OVA': 0, 'OITGA': 0.0, 'ORP': 0, 'OVP': 0,\
+            'OITGP': 0.0, 'IR': 23064358, 'IV': 0, 'IITG': 23064358.0, 'DT':\
+            '2010-05-01', 'PRIZ': 1, 'P_K':1})
+
+        
 
     def test_one_hundred_report_endpoint(self):
         """
         test for one_hundred_report
         """
         logging.info('\nStart endpoint test..')
-        resp = json.loads(self._client.get('/one_hundred_report').\
+        DB.add(OneHundredReport(**self._data))
+        resp = json.loads(self._client.get('/one_hundred_report/1').\
             get_data().decode())
-        print(resp)
+        self.assertListEqual(
+            [{'A_P': '2', 'DT': '2010-05-01T00:00:00', 'IITG': 23064358, 'IR':\
+            23064358.0, 'IV': 0.0, 'NUM_SC': '10207', 'OITGA': 0.0, 'OITGP': 0.0, 'ORA':\
+            0.0, 'ORP': 0.0, 'OVA': 0.0, 'OVP': 0.0, 'PLAN': 'А', 'PRIZ': 1, 'P_K': 1,\
+            'REGN': 1, 'VITG': 23064358.0, 'VR': 23064358.0, 'VV': 0.0}, {'A_P': '2', 'DT':\
+            '2010-05-01T00:00:00', 'IITG': 23064358, 'IR': 23064358.0, 'IV': 0.0, 'NUM_SC':\
+            '10207', 'OITGA': 0.0, 'OITGP': 0.0, 'ORA': 0.0, 'ORP': 0.0, 'OVA': 0.0, 'OVP':\
+            0.0, 'PLAN': 'А', 'PRIZ': 1, 'P_K': 2, 'REGN': 1, 'VITG': 23064358.0, 'VR':
+            23064358.0, 'VV': 0.0}], resp)
 
 if __name__ == "__main__":
     unittest.main()
--- api/src/utils.py
+++ api/src/utils.py
@@ -1,4 +1,20 @@
 """
     Here usefull utils
 """
-#TODO write convertaiton model to dict
+from decimal import Decimal
+import datetime
+
+def model_to_dict(model):
+    """
+    extract model to dict
+    """
+    out = dict()
+    for key in model.__dict__:
+        if key != '_sa_instance_state':
+            out[key] = model.__dict__.get(key)
+            if isinstance(model.__dict__.get(key), datetime.date):
+                out[key] = model.__dict__.get(key).isoformat()
+            if isinstance(model.__dict__.get(key), Decimal):
+                out[key] = float(model.__dict__.get(key))
+    return out
+        
