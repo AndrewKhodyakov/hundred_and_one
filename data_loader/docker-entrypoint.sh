1965d864f0d0c76e7b4364d4a1ef5240f145c0cf
ajet <andrew.khodyakov@bostongene.com>
2018-04-23 12:12:51 +0300

    Continue write loader setup

---
+++ data_loader/docker-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#start command
+START_PROD_SERVER="gunicorn --bind=$HOST_IP:$SERVER_PORT --workers=$WORKER_COUNT --timeout=$REQ_TIMEOUT app:app"
+
+cd $BASE_DIR
+
+if [ $1 = 'up' ] ; then
+    #here database initialization
+    $START_PROD_SERVER
+else
+    echo 'Set start mode in $1!'
+    exit 1
+fi
